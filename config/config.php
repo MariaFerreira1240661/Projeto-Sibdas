ef28509632ea17e51e9d173eac702b81357ccaef
MariaFerreira1240661 <1240661@isep.ipp.pt>
2026-06-15 00:16:53 +0100

    Criar ficheiro de configuração global

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+// --------------------------------------------------------------------
+// CONFIGURAÇÕES GLOBAIS DA APLICAÇÃO
+// --------------------------------------------------------------------
+
+define('APP_NAME', 'MedControl');
+define('APP_VERSION', '1.0.0');
+define('APP_COPYRIGHT', '© 2026 MedControl - Sistema de Inventário Hospitalar');
