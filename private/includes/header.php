fc23e48d5359bb896f124db9729655bd192dcf56
MariaFerreira1240661 <1240661@isep.ipp.pt>
2026-06-15 09:33:06 +0100

    criar footer com scripts comuns da área privada

---
+++ private/includes/header.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../../config/config.php';
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - <?php echo APP_NAME; ?></title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../assets/img/medcontrol-logo2.png" type="image/png">
+
+    <!-- Bootstrap -->
+    <link href="../assets/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="../assets/bootstrap/bootstrap-icons.min.css">
+
+    <!-- CSS do projeto -->
+    <link rel="stylesheet" href="../assets/css/1240661.css">
+</head>
+
+<body class="backend-body">
--- private/index.php
+++ private/index.php
@@ -1,28 +1,4 @@
-<?php
-require_once __DIR__ . '/../config/config.php';
-?>
-
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - <?php echo APP_NAME; ?></title>
-
-    <!-- favicon -->
-    <link rel="shortcut icon" href="../assets/img/medcontrol-logo2.png" type="image/png">
-
-    <!-- Bootstrap -->
-    <link href="../assets/bootstrap/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="../assets/bootstrap/bootstrap-icons.min.css">
-
-    <!-- CSS do projeto -->
-    <link rel="stylesheet" href="../assets/css/1240661.css">
-</head>
-
-<body class="backend-body">
+<?php include 'includes/header.php'; ?>
 
     <div class="backend-layout">
 
@@ -486,6 +462,3 @@ require_once __DIR__ . '/../config/config.php';
     <script src="../assets/bootstrap/bootstrap.bundle.min.js"></script>
     <script src="../assets/js/chart.js"></script>
     <script src="../assets/js/1240661.js"></script>
-
-</body>
-</html>
